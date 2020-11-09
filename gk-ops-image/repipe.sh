4c17b09771c976ece767050732b909682084adcf
Benjamin Gandon <benjamin@gandon.org>
2020-11-09 02:40:20 +0100

    Improve repipe script

--- gk-ops-image/repipe.sh
+++ gk-ops-image/repipe.sh
@@ -4,9 +4,15 @@ set -eo pipefail -u
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+team_name="$(   bosh interpolate --path "/team_name"     "${SCRIPT_DIR}/config.yml")"
 pipeline_name=$(bosh interpolate --path "/pipeline_name" "${SCRIPT_DIR}/config.yml")
 
-fly --target="gk" \
-    set-pipeline --pipeline="${pipeline_name}" \
-    --config="${SCRIPT_DIR}/pipeline.yml" \
-    --load-vars-from="${SCRIPT_DIR}/config.yml"
+pushd "${SCRIPT_DIR}" > /dev/null
+(
+    set -x
+    fly --target="${team_name}" \
+        set-pipeline --pipeline="${pipeline_name}" \
+        --config="pipeline.yml" \
+        --load-vars-from="config.yml"
+)
+popd > /dev/null
