b792ceb1c9ddfed1305638a2ca17e625e571de97
Benjamin Gandon <benjamin@gandon.org>
2020-11-09 01:13:59 +0100

    Allow specifying Git branch explicitely using a parameter

--- boshrelease/git-push.sh
+++ boshrelease/git-push.sh
@@ -9,10 +9,17 @@ touch "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa"
 cat <<< "${GITHUB_PRIVATE_KEY}" > "${HOME}/.ssh/id_rsa"
 
-grep -vE "^(UPDATED|UUID)=" "branch-info/keyval.properties" \
-    | sed -r -e 's/"/\"/g; s/=(.*)$/="\1"/' \
-    > keyval.inc.bash
-source "keyval.inc.bash"
+if [[ -n ${BRANCH_NAME} ]]
+    branch_name="${BRANCH_NAME}"
+elif [[ -f "branch-info/keyval.properties" ]]; then
+    grep -vE "^(UPDATED|UUID)=" "branch-info/keyval.properties" \
+        | sed -r -e 's/"/\"/g; s/=(.*)$/="\1"/' \
+        > keyval.inc.bash
+    source "keyval.inc.bash"
+else
+    echo "ERROR: no 'branch-info' resource, and no 'BRANCH_NAME' param. One must be specified. Aborting." >&2
+    exit 2
+fi
 
 pushd "repo" > /dev/null
     git remote set-url "origin" "${GIT_URI}"
