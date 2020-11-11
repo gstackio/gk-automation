4146e81e42a7ab7c755dd9b5c55096ec5f6d1ca4
Benjamin Gandon <benjamin@gstack.io>
2020-11-11 23:53:44 +0100

    Add support for pushing to Bitbucket

--- boshrelease/git-push.sh
+++ boshrelease/git-push.sh
@@ -23,7 +23,7 @@ fi
 
 pushd "repo" > /dev/null
     git remote set-url "origin" "${GIT_URI}"
-    ssh-keyscan -t "rsa" "github.com" 2> /dev/null >> "${HOME}/.ssh/known_hosts"
+    ssh-keyscan -t "rsa" "bitbucket.org" "github.com"    2> /dev/null >> "${HOME}/.ssh/known_hosts"
     (
         set -x
         git push --set-upstream "origin" "${branch_name}"
