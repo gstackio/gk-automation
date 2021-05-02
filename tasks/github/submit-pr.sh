392ebb3fbdb46d2125214a00719c4784740fad11
Benjamin Gandon <benjamin@gstack.io>
2021-05-02 15:30:32 +0200

    Improve code formatting

--- tasks/github/submit-pr.sh
+++ tasks/github/submit-pr.sh
@@ -38,18 +38,20 @@ if [[ ${open_pulls_count} -gt 0 ]]; then
 fi
 
 # See also: https://developer.github.com/v3/pulls/#create-a-pull-request
-pr_data=$(jq -n \
-    --arg "title" "${pr_title}" \
-    --arg "body"  "${pr_desc}" \
-    --arg "base"  "${base_branch}" \
-    --arg "head"  "${branch_name}" \
-    '{
-        "title": $title,
-        "body":  $body,
-        "base":  $base,
-        "head":  $head,
-        "maintainer_can_modify": true
-    }')
+pr_data=$(
+    jq --null-input \
+        --arg "title" "${pr_title}" \
+        --arg "body"  "${pr_desc}" \
+        --arg "base"  "${base_branch}" \
+        --arg "head"  "${branch_name}" \
+        '{
+            "title": $title,
+            "body":  $body,
+            "base":  $base,
+            "head":  $head,
+            "maintainer_can_modify": true
+        }'
+)
 
 echo "Creating pull request: POST /repos/${GH_OWNER}/${GH_REPO}/pulls"
 # See also: https://developer.github.com/v3/
