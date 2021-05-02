b70ef243d3040c43b68df34ef5da28d6002572ab
Benjamin Gandon <benjamin@gstack.io>
2021-05-02 11:15:32 +0200

    Fix incompatible 'date' CLI arguments + Improve artifact URL computation

--- tasks/github/force-release-artifact-timestamp.sh
+++ tasks/github/force-release-artifact-timestamp.sh
@@ -2,28 +2,30 @@
 
 set -ueo pipefail
 
-: ${GH_OWNER:?required}
-: ${GH_REPO:?required}
 : ${ARTIFACT_FILE:?required}
 
-tag=$(< github-release/tag)
+release_url=$(< github-release/url)
 
-url="https://github.com/${GH_OWNER}/${GH_REPO}/releases/download/${tag}/${ARTIFACT_FILE}"
+artifact_url=$(
+    sed -e 's|/tag/|/download/|' -e "s|$|/${ARTIFACT_FILE}|" <<< "${release_url}"
+)
 
 blob_last_modified=$(
-    curl --silent --fail --show-error --location --head --url "${url}" \
+    curl --silent --fail --show-error --location --head --url "${artifact_url}" \
         | grep -i '^last-modified:' \
         | tail -n1 \
         | cut -d" " -f "2-" \
         | tr -d '\r'
 )
 
-date_utc=$(date -j -f "%a, %d %b %Y %T %Z" "${blob_last_modified}" "+%F %T")
+# date_utc=$(date -j -f "%a, %d %b %Y %T %Z" "${blob_last_modified}" "+%F %T") # Darwin
+date_utc=$(date --date="${blob_last_modified}" "+%F %T") # Linux
 echo "INFO: setting timestamps to '${date_utc}' on file '${ARTIFACT_FILE}'."
 
 # time format for 'touch' command: [[CC]YY]MMDDhhmm[.SS]
 last_modified_ts=$(
-    date -j -f "%a, %d %b %Y %T %Z" "${blob_last_modified}" "+%Y%m%d%H%M.%S"
+    # date -j -f "%a, %d %b %Y %T %Z" "${blob_last_modified}" "+%Y%m%d%H%M.%S" # Darwin
+    date --date="${blob_last_modified}" "+%Y%m%d%H%M.%S" # Linux
 )
 
 cp -a github-release/* github-release-timestamped/
