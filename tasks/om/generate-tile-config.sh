817e0abce4767c04ed060b149215efeb26a05ca3
Benjamin Gandon <benjamin@gstack.io>
2021-05-11 23:23:12 +0200

    Add task that generates config files for some tile

---
+++ tasks/om/generate-tile-config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+: ${PIVNET_PRODUCT_SLUG:?"required"}
+: ${PIVNET_PRODUCT_VERSION:?"required"}
+: ${PIVNET_PRODUCT_FILE_GLOB:?"required"}
+: ${PIVNET_API_TOKEN:?"required"}
+: ${OUTPUT_DIR:?"required"}
+
+find "repo" -mindepth 1 -maxdepth 1 -print0 \
+    | xargs -0 -I{} cp -a {} "repo-updated"
+
+pushd "repo-updated" > /dev/null
+
+    mkdir -p "${OUTPUT_DIR}"
+
+    om config-template \
+        --pivnet-product-slug="${PIVNET_PRODUCT_SLUG}" \
+        --product-version="${PIVNET_PRODUCT_VERSION}" \
+        --product-file-glob="${PIVNET_PRODUCT_FILE_GLOB}" \
+        \
+        --pivnet-api-token="${PIVNET_API_TOKEN}" \
+        --exclude-version \
+        --output-directory="${OUTPUT_DIR}"
+
+popd > /dev/null
